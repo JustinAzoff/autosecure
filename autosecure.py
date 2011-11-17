b801c13c814adb2638f0d75fe17f55fbe6091c07
Justin <justin@bouncybouncy.net>
2011-11-16 19:13:13 -0500

    extract UA

--- autosecure.py
+++ autosecure.py
@@ -1,5 +1,7 @@
 import getopt, sys, pcap, dpkt, re, httplib, urllib
 
+DEFAULT_UA = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; MathPlayer 2.10b; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729"
+
 def re_extract(rex, data):
     m = re.search(rex, data)
     if m:
@@ -33,9 +35,13 @@ class AutoSecure:
         cookie = re_extract('Cookie: ([^\r\n]+)', packet)
         if not cookie:
             return
+        ua = re_extract('User-Agent: ([^\r\n]+)', packet)
+        if not ua:
+            ua = DEFAULT_UA
         return {
-            'host': host,
-            'cookie': cookie,
+            'Host': host,
+            'Cookie': cookie,
+            'User-Agent': ua,
         }
 
     def secure_sheep(self):
