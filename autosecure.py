7bb2408d88fae3bc5502b203a2394a6aed22c57c
Justin <justin@bouncybouncy.net>
2011-11-27 23:14:47 -0500

    remove DEFAULT_UA

--- autosecure.py
+++ autosecure.py
@@ -2,9 +2,6 @@ import getopt, sys, pcap, dpkt, re, httplib
 import requests
 from pyquery import PyQuery as pq
 
-
-DEFAULT_UA = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; MathPlayer 2.10b; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729"
-
 def re_extract(rex, data):
     m = re.search(rex, data)
     if m:
