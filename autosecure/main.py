cb49129c5848b9468030dd25bf3f1940d71477c2
Justin Azoff <justin@bouncybouncy.net>
2011-11-29 14:40:01 -0500

    rewrite to use libnids
    large cookies cause the http request to be fragmented into multiple
    packets, which requires stream reassembly. libnids does this.

--- autosecure/main.py
+++ autosecure/main.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!usr/bin/env python
+import os
+import pwd
 import re
 import sys
 
-import pcap
-import dpkt
+import nids
 
 import requests
 from pyquery import PyQuery as pq
@@ -11,30 +12,48 @@ from pyquery import PyQuery as pq
 from autosecure.handlers import handler_map
 from autosecure.util import re_extract
 
-
 DEFAULT_UA = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; MathPlayer 2.10b; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729"
 
 class AutoSecure:
-    def __init__(self, interface="wlan0"):
+    def __init__(self, interface="wlan0", uid="nobody"):
         self.interface = interface
         self.secured_users = set()
 
-    def get_packets(self):
-        cap = pcap.pcap(self.interface)
-        cap.setfilter('dst port 80')
-        for ts, raw in cap:
-            eth = dpkt.ethernet.Ethernet(raw)
-            if isinstance(eth.data, str):
-                data = eth.data
-            else:
-                data = eth.data.data.data
-            yield ts, data
+        self.nids_init()
+        self.drop_privs(uid)
 
-    def get_sessions(self):
-        for ts, data in self.get_packets():
-            session = self.extract_session(data)
-            if session:
-                yield session
+    def nids_init(self):
+        nids.param("device", self.interface)
+        nids.param("pcap_filter", "port 80")
+        nids.param("san_num_hosts", 0)
+        #nids.param("dev_addon", ?) #might need this for monitor mode
+        nids.chksum_ctl([('0.0.0.0/0', False)]) # disable checksumming
+        nids.init()
+        nids.register_tcp(self.handle_tcp)
+        self.nids = nids
+        
+
+    def drop_privs(self, uid):
+        (uid, gid) = pwd.getpwnam(uid)[2:4]
+        os.setgroups([gid,])
+        os.setgid(gid)
+        os.setuid(uid)
+        if 0 in [os.getuid(), os.getgid()] + list(os.getgroups()):
+            raise Exception("error - drop root, please!")
+
+    def handle_tcp(self, stream):
+        if stream.nids_state == nids.NIDS_JUST_EST:
+            ((src, sport), (dst, dport)) = stream.addr
+            if dport == 80:
+                stream.server.collect = 1
+            return
+        elif stream.nids_state == nids.NIDS_DATA:
+            stream.discard(0)
+
+        bytes = stream.server.data[:stream.server.count]
+        session = self.extract_session(bytes)
+        if session:
+            self.secure_sesion(session)
 
     def extract_session(self, packet):
         host = re_extract('Host: ([^\r\n]+)[\r\n]', packet)
@@ -65,9 +84,7 @@ class AutoSecure:
         return h.secure(session)
 
     def secure_sheep(self):
-        for s in self.get_sessions():
-            print s
-            self.secure_sesion(s)
+        self.nids.run()
 
 def main():
     from optparse import OptionParser
